596d5cf3a9feb6069dc3c9720a570cb489068453
demidraksin <greatraksin@icloud.com>
2021-08-20 14:31:24 +0300

    Adding game objects⭐️

--- objects.py
+++ objects.py
@@ -5,14 +5,18 @@ import pygame as pg
 
 
 class GameRound:
+    # TODO: make a main Game Object.
     pass
 
 
 class Snake:
+    # TODO: choose snake position and appearance,
+    #  add eating method
     pass
 
 
 class Food:
+    # TODO: initialize food, choose food position
     pass
 
 
