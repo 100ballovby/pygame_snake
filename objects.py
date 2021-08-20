42ebf610deb788c3092ab2e98671f321013c34a9
demidraksin <greatraksin@icloud.com>
2021-08-20 14:37:45 +0300

    declare check_errors method🍕

--- objects.py
+++ objects.py
@@ -11,6 +11,11 @@ class GameRound:
 
     @staticmethod
     def check_errors():
+        """
+        Initializing pygame and checking for errors.
+        If erorr exists, crash programm.
+        :return: None
+        """
         check = pg.init()  # Инициализирую pygame
         if check[1] > 0:  # если есть коды ошибок
             sys.exit()  # закрыть приложение
