b0884cd093e4ba55f8515a9cd26b1275c8264bb5
demidraksin <greatraksin@icloud.com>
2021-08-20 14:27:42 +0300

    Adding game objects⭐️

---
+++ objects.py
@@ -0,0 +1,22 @@
+import random
+import time
+import sys
+import pygame as pg
+
+
+class GameRound:
+    pass
+
+
+class Snake:
+    pass
+
+
+class Food:
+    pass
+
+
+if __name__ == '__main__':
+    game = GameRound()
+    snake = Snake()
+    food = Food()
